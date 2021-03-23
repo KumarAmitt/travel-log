3f7a1d17dec8c8b96c39f6b46f4f5c12d11735be
KumarAmitt <amitt2015@outlook.com>
2021-03-23 17:56:52 +0530

    Travel Controller for selecting group

--- app/controllers/travels_controller.rb
+++ app/controllers/travels_controller.rb
@@ -19,7 +19,7 @@ class TravelsController < ApplicationController
     @travel = current_user.travels.build(travel_params)
 
     if @travel.save
-      # @travel.travel_groups.create(show_group_id)
+      @travel.travel_groups.create(show_group_id)
       redirect_to travels_path, notice: 'Travel was successfully created.'
     else
       render 'new', status: :unprocessable_entity
@@ -29,6 +29,15 @@ class TravelsController < ApplicationController
 
   def update
     if @travel.update(travel_params)
+
+      if show_group_id[:group_id]
+        if @travel.groups.empty?
+          @travel.travel_groups.create(show_group_id)
+        else
+          @travel.travel_groups.update(show_group_id)
+        end
+      end
+
       redirect_to @travel, notice: "Travel was successfully updated."
     else
       render :edit, status: :unprocessable_entity
@@ -51,7 +60,7 @@ class TravelsController < ApplicationController
   end
 
   def show_group_id
-    params.require(:transaction).permit(:group_id)
+    params.require(:travel).permit(:group_id)
   end
 
 end
--- app/views/travels/_travel.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! travel, :id, :purpose, :source, :destination, :distance, :user_id, :created_at, :updated_at
-json.url travel_url(travel, format: :json)
