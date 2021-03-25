d84f3ceb1b757a409e08b02503270fb87178eb48
KumarAmitt <amitt2015@outlook.com>
2021-03-25 21:30:49 +0530

    Add styles for travel/index view

--- app/helpers/travels_helper.rb
+++ app/helpers/travels_helper.rb
@@ -1,2 +1,9 @@
 module TravelsHelper
+  def travel_icon(travel)
+    if travel.groups.first.nil?
+      image_tag '/assets/travel_icon.jpg', class: 'travel_item_icon'
+    else
+      image_tag travel.groups.first.icon, class: 'travel_item_icon'
+    end
+  end
 end
