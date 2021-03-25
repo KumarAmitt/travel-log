47ad5036865c7a2c25f69e11bcc0ed3886ce532f
KumarAmitt <amitt2015@outlook.com>
2021-03-25 17:40:25 +0530

    Add styles for users/profile

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,9 +2,9 @@ module UsersHelper
 
   def profile_avatar
     if current_user.profile_pic.attached?
-      image_tag current_user.profile_pic
+      image_tag current_user.profile_pic, class: 'circle responsive-img profile_img'
     else
-      image_tag '/assets/blank_profile.png'
+      image_tag '/assets/blank_profile.png', class: 'circle responsive-img profile_img'
     end
   end
 
