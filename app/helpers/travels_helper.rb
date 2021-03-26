b80aa60d58c1cc144835ff678ab9cbb55a5a186e
KumarAmitt <amitt2015@outlook.com>
2021-03-26 21:48:44 +0530

    Update image path

--- app/helpers/travels_helper.rb
+++ app/helpers/travels_helper.rb
@@ -1,9 +1,9 @@
 module TravelsHelper
   def travel_icon(travel)
     if travel.groups.first.nil?
-      image_tag '/assets/travel_icon.jpg', class: 'travel_item_icon'
+      image_tag 'travel_icon.jpg', class: 'travel_item_icon'
     elsif !travel.groups.first.icon.attached?
-      image_tag '/assets/travel_icon.jpg', class: 'travel_item_icon'
+      image_tag 'travel_icon.jpg', class: 'travel_item_icon'
     else
       image_tag travel.groups.first.icon, class: 'travel_item_icon'
     end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,7 +3,7 @@ module UsersHelper
     if current_user.profile_pic.attached?
       image_tag current_user.profile_pic, class: 'circle responsive-img profile_img'
     else
-      image_tag '/assets/blank_profile.png', class: 'circle responsive-img profile_img'
+      image_tag 'blank_profile.png', class: 'circle responsive-img profile_img'
     end
   end
 end
