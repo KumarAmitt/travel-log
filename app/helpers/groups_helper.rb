e3bb319fa015d1a46184304dbd399fff86de6620
KumarAmitt <amitt2015@outlook.com>
2021-03-26 10:36:42 +0530

    Add styles for groups/show & _form partial

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -2,9 +2,9 @@ module GroupsHelper
 
   def group_icon(group)
     if group.icon.attached?
-      image_tag group.icon
+      image_tag group.icon, class: 'travel_item_icon'
     else
-      image_tag '/assets/travel_icon.jpg'
+      image_tag '/assets/travel_icon.jpg', class: 'travel_item_icon'
     end
   end
 
